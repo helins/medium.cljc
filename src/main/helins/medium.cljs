fbc73cec86174df6cddcb045ee7628e2d06cf1b7
Adam Helinski <adam@dvlopt.io>
2021-01-29 18:34:12 +0100

    Add function equivalents to current macros

---
+++ src/main/helins/medium.cljs
@@ -0,0 +1,13 @@
+(ns helins.medium
+
+  ""
+
+  {:author "Adam Helinski"}
+
+  (:require-macros [helins.medium :refer [base-cljs?*
+                                          base-clojure?*
+                                          cljs-level*
+                                          cljs-level-raw*
+                                          compile-cljs?*
+                                          compile-clojure?*
+                                          refresh-clojure*]]))
